d2a004121bce7200c2cb4613e129f9cb5c8f33bf
github username <github email address>
2019-03-18 15:25:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+
+let totalBatteries = 0
+let batteryBatches = [10, 9, 8, 4]
+
+totalBatteries = batteryBatches.reduce((totalBatteries, battery) => {
+  return totalBatteries + battery
+}, 0)
