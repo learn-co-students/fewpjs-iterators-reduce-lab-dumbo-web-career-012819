1b684c3c31c5519ed6e66a29bb5ce13e472fc691
github username <github email address>
2019-03-25 07:54:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution here
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+let arr = [4, 5, 3, 4, 4, 6, 5]
+
+const totalBatteries = arr.reduce(reducer)
